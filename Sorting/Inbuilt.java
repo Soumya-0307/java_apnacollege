194cbdbda6ee24f590fd8d6836ed293e4f4703bf
soumya Hegde <soumyahegde652@gmail.com>
2024-07-25 15:15:23 +0530

    inbuit sorting with collections and arrays

---
+++ Sorting/Inbuilt.java
@@ -0,0 +1,29 @@
+package Sorting;
+import java.util.Arrays;
+import java.util.Collections;
+public class Inbuilt {
+    public static void main(String args[]){
+   Integer arrays[]={5,8,2,3,9,1};
+   Arrays.sort(arrays);
+   for(int i=0;i<arrays.length;i++){
+    System.out.print(arrays[i]+" ");
+   }
+   System.out.println();
+
+   System.out.println("arrays in reverse order:");
+   Arrays.sort(arrays,Collections.reverseOrder());
+   for(int i=0;i<arrays.length;i++){
+    System.out.print(arrays[i]+" ");
+   }
+
+   System.out.println();
+
+   System.out.println("arrays in reverse order with index:");
+   Arrays.sort(arrays,0,4,Collections.reverseOrder());
+   for(int i=0;i<arrays.length;i++){
+    System.out.print(arrays[i]+" ");
+   }
+
+
+    }
+}
